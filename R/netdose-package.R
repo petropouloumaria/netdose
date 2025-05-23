5def488eb12805af7f11b040dca015e503b58df0
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2025-05-23 16:13:30 +0200

    dotplot(): use either 'geom_dotplot()' or 'geom_point()'

--- R/netdose-package.R
+++ R/netdose-package.R
@@ -44,11 +44,15 @@
 #' @importFrom MASS ginv
 #' @importFrom stats predict quantile median pchisq optimize
 #' @importFrom Matrix bdiag
-#' @importFrom ggplot2 ggplot aes
+#' @importFrom ggplot2 ggplot aes coord_cartesian
 #'   element_blank element_line element_rect element_text
-#'   facet_wrap geom_line geom_dotplot geom_point geom_ribbon geom_vline
-#'   labs ylab scale_color_manual theme theme_minimal 
-#' @importFrom dplyr %>% mutate select distinct filter rename
+#'   facet_wrap geom_dotplot geom_line geom_point geom_ribbon geom_text
+#'   geom_vline guide_axis labs scale_color_manual
+#'   scale_x_continuous scale_y_continuous theme theme_minimal
+#' @importFrom ggh4x facet_wrap2 facetted_pos_scales
+#' @importFrom dplyr %>% arrange count distinct filter group_by if_else
+#'   left_join mutate n n_distinct pull rename row_number select
+#'   summarise ungroup
 #' @importFrom tidyr pivot_longer
 
 "_PACKAGE"
